1c90eed1d853e3d6b4e05593712dad194637c68d
edmobe <31488944+edmobe@users.noreply.github.com>
2020-10-26 02:40:56 -0600

    Switch back to arrays only for simplicity

--- aliens.c
+++ aliens.c
@@ -14,9 +14,30 @@ struct Alien
     int direction;
     int energy;
     int period;
-    struct Alien *next;
     pthread_t threadId;
 };
+
+struct AlienArray
+{
+    struct Alien aliens[MAX_PROCESSES];
+    int length;
+};
+
+void initialize(struct AlienArray *alienArray)
+{
+    alienArray->length = 0;
+}
+
+float getUtilization(struct AlienArray *alienArray)
+{
+    float utilization = 0;
+    for (int i = 0; i < alienArray->length; i++)
+    {
+        if (!alienArray->aliens[i].finished)
+            utilization += (float)alienArray->aliens[i].energy / (float)alienArray->aliens[i].period;
+    }
+    return utilization;
+}
 
 void *
 print(void *arg)
@@ -24,58 +45,11 @@ print(void *arg)
     printf("Thread initialized, energy = %d\n", *((int *)arg));
 }
 
-/* Given a reference (pointer to pointer) to the head 
-   of a list and an int, appends a new node at the end 
-
-    AlgorithmId:
-        0: RM
-        1: EDF
-*/
-int append(struct Alien **head_ref, int energy, int period)
+int append(struct AlienArray *alienArray, int energy, int period)
 {
-    /* 1. allocate node */
-    struct Alien *newAlien = (struct Alien *)malloc(sizeof(struct Alien));
-
-    struct Alien *last = *head_ref;                    /* used in step 5*/
-    int len = 0;                                       /* used in step 5*/
-    float utilization = (float)energy / (float)period; /* Determines if the new process can be scheduled */
-
-    /* 2. put in the data  */
-    newAlien->energy = energy;
-    newAlien->period = period;
-    newAlien->direction = 'r';
-    newAlien->finished = 0;
-    newAlien->posX = 0;
-    newAlien->posY = 7;
-
-    /* 3. This new node is going to be the last node, so make next  
-          of it as NULL */
-    newAlien->next = NULL;
-
-    /* 4. If the Linked List is empty, then make the new node as head */
-    if (*head_ref == NULL)
-    {
-        if (utilization >= 1)
-        {
-            printf("Error: proccess is not planificable.\n");
-            return 0;
-        }
-        pthread_create(&newAlien->threadId, NULL, print, &newAlien->energy);
-        pthread_join(newAlien->threadId, NULL);
-        *head_ref = newAlien;
-        return 1;
-    }
-
-    utilization += (float)last->energy / (float)last->period;
-    len++;
-
-    /* 5. Else traverse till the last node */
-    while (last->next != NULL)
-    {
-        last = last->next;
-        utilization += (float)last->energy / (float)last->period;
-        len++;
-    }
+    /* Determines if the new process can be scheduled */
+    float utilization = getUtilization(alienArray);
+    utilization += (float)energy / (float)period;
 
     if (utilization >= 1)
     {
@@ -83,31 +57,38 @@ int append(struct Alien **head_ref, int energy, int period)
         return 0;
     }
 
-    if (len + 1 > MAX_PROCESSES)
+    if (alienArray->length + 1 > MAX_PROCESSES)
     {
         printf("Error: max amount of proccesses is %d.\n", MAX_PROCESSES);
         return 0;
     }
 
-    /* 6. Start thread */
-    pthread_create(&newAlien->threadId, NULL, print, &newAlien->energy);
-    pthread_join(newAlien->threadId, NULL);
+    printf("Utilization: %f\n", utilization);
 
-    /* 7. Change the next of last node */
-    last->next = newAlien;
+    alienArray->aliens[alienArray->length].energy = energy;
+    alienArray->aliens[alienArray->length].period = period;
+    alienArray->aliens[alienArray->length].direction = 'r';
+    alienArray->aliens[alienArray->length].finished = 0;
+    alienArray->aliens[alienArray->length].posX = 0;
+    alienArray->aliens[alienArray->length].posY = 7;
+    pthread_create(&alienArray->aliens[alienArray->length].threadId, NULL, print, &alienArray->aliens[alienArray->length].energy);
+    pthread_join(alienArray->aliens[alienArray->length].threadId, NULL);
+    alienArray->length++;
 
     return 1;
 }
 
-// This function prints contents of linked list starting from head
-void printAlienList(struct Alien *alien)
+// This function prints contents of the alien array
+void printAlienArray(struct AlienArray *alienArray)
 {
     float sum = 0;
-    while (alien != NULL)
+    for (int i = 0; i < alienArray->length; i++)
     {
-        printf("[%d, %d] ", alien->energy, alien->period);
-        sum += (float)alien->energy / (float)alien->period;
-        alien = alien->next;
+        if (!alienArray->aliens[i].finished)
+        {
+            printf("[%d, %d] ", alienArray->aliens[i].energy, alienArray->aliens[i].period);
+            sum += (float)alienArray->aliens[i].energy / (float)alienArray->aliens[i].period;
+        }
     }
     printf("\nUtilization: %f\n", sum);
 }
@@ -134,27 +115,15 @@ void printMaze(int maze[DIMENSIONS][DIMENSIONS])
 int main()
 {
     /* Start with the empty list */
-    struct Alien *head = NULL;
+    struct AlienArray alienArray;
+    initialize(&alienArray);
 
-    /*
-    // Test
-    append(&head, 2, 9, 1);
-    append(&head, 3, 20, 1);
-    append(&head, 5, 30, 1);
-    append(&head, 4, 20, 1);
-    append(&head, 10, 25, 1);
-    append(&head, 1, 50, 1);
-    append(&head, 2, 15, 1);
-    append(&head, 1, 18, 1);
-    append(&head, 2, 15, 1);
-    */
-
-    append(&head, 1, 6);
-    append(&head, 2, 9);
-    append(&head, 6, 18);
+    append(&alienArray, 1, 6);
+    append(&alienArray, 2, 9);
+    append(&alienArray, 17, 18);
 
     printf("Created Linked list is: ");
-    printAlienList(head);
+    printAlienArray(&alienArray);
     printf("\n");
 
     int maze[DIMENSIONS][DIMENSIONS] =
